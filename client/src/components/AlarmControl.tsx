0c734f1e00f9d40d5250ae01cb09a93d752b2705
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-12-17 22:26:19 +0800

    Add skeleton for alarm and smoke detector

---
+++ client/src/components/AlarmControl.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AlarmControl = () => {
+  return <div>AlarmControl</div>;
+};
+
+export default AlarmControl;
---
+++ server/modules/gpio-handler.py
@@ -0,0 +1,7 @@
+import RPi.GPIO as GPIO
+
+class GPIOHandler:
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        # Add GPIO setup here
+        pass
