17c8750d81dd28f08592a3c299277d86c05307e7
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-12-18 13:48:56 +0800

    Refactor tailwind config js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,21 +6,3 @@ module.exports = {
   },
   plugins: [],
 };
-
-// vite.config.ts
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-    proxy: {
-      "/socket.io": {
-        target: "http://localhost:5000",
-        ws: true,
-      },
-    },
-  },
-});
