68b2b3cfb5dd2ef91707b78bd31e5c74cef1028d
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-12-18 16:06:20 +0800

    Change GPIO pins

--- server/app.py
+++ server/app.py
@@ -12,8 +12,8 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 
 # Initialize handlers
 camera_handler = CameraHandler(fps=20)
-alarm_handler = AlarmHandler(alarm_pin=18)
-gpio_handler = GPIOHandler(smoke_detector_pin=17)
+alarm_handler = AlarmHandler(alarm_pin=12)
+gpio_handler = GPIOHandler(smoke_detector_pin=11)
 data_handler = DataHandler(gpio_handler, alarm_handler, interval=1.0)
 
 def handle_smoke_detection(is_smoke_detected):
